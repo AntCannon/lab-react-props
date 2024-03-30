200f5e727a0f29c12b54544a4aebc603bf9dddd3
AntCannon <anthonycannonier@gmail.com>
2024-03-30 13:16:50 -0400

    add donation styling

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,7 +1,9 @@
+import './RecentDonations.css';
+
 export default function RecentDonations({donation}) {
   const { name, amount, caption, id } = donation;
   return (
-    <div className="recent-donations">
+    <div className="donation">
       <span className="name-amount">{name} donated ${amount}</span><br />
       <span>{caption}</span>
     </div>
