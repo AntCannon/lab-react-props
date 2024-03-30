64fab7f3c032e0b666a52ee036313b9477992ca5
AntCannon <anthonycannonier@gmail.com>
2024-03-30 14:26:26 -0400

    attempt donation progress bar

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,11 +1,16 @@
-
+import './Progress.css';
 
 export default function Progress({ totalRaised, targetAmount }) {
+  const raisedPercent = totalRaised / targetAmount
+  console.log(raisedPercent)
 
   return (
     <section className="progress">
       <h2>Raised <span className="total-raised">${totalRaised}</span> of <span className="target-amount">${targetAmount}</span></h2>
-      <div className="progress-bar"></div>
+      <div className="progress-bar">
+        <div className="raised-percent" width={raisedPercent+"%"}></div>
+        <progress value={raisedPercent} />
+      </div>
     </section>
   );
 }
