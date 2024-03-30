5b34421245251d77b90037a44d53cde535a6a12a
AntCannon <anthonycannonier@gmail.com>
2024-03-30 15:08:18 -0400

    update progress bar

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -7,10 +7,13 @@ export default function Progress({ totalRaised, targetAmount }) {
   return (
     <section className="progress">
       <h2>Raised <span className="total-raised">${totalRaised}</span> of <span className="target-amount">${targetAmount}</span></h2>
-      <div className="progress-bar">
+      {/* progress bar */}
+      {/* https://dev.to/ramonak/react-how-to-create-a-custom-progress-bar-component-in-5-minutes-2lcl */}
+      {/* <div className="progress-bar">
         <div className="raised-percent" width={raisedPercent+"%"}></div>
-        <progress value={raisedPercent} />
-      </div>
+      </div> */}
+      {/* https://react.dev/reference/react-dom/components/progress */}
+      <progress className="progress-component" value={raisedPercent} />
     </section>
   );
 }
